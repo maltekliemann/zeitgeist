a3f175a53146f077c9863a95017092cb42487607
maltekliemann <mail@maltekliemann.com>
2022-04-21 23:39:48 +0200

    Substrate is out of the picture

--- zrml/prediction-markets/src/lib.rs
+++ zrml/prediction-markets/src/lib.rs
@@ -1,12 +1,14 @@
-use frame_support::storage::{with_transaction, TransactionOutcome};
+pub fn with_transaction(f: impl FnOnce() -> i32) -> i32 {
+    f()
+}
 
 pub enum MultiHash {
     Sha3_384([u8; 50]),
 }
 
-pub fn create_categorical_market(metadata: MultiHash) -> Option<u128> {
+pub fn create_categorical_market(metadata: MultiHash) -> i32 {
     with_transaction(|| {
         let MultiHash::Sha3_384(_multihash) = metadata;
-        TransactionOutcome::Commit(Some(0))
+        0i32
     })
 }
