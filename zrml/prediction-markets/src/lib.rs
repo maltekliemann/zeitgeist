264670c653139c93596a7551cf6a4aff9697dbed
maltekliemann <mail@maltekliemann.com>
2022-04-21 23:31:39 +0200

    Keep reducing

--- zrml/prediction-markets/src/lib.rs
+++ zrml/prediction-markets/src/lib.rs
@@ -1,49 +1,25 @@
-extern crate alloc;
+#[cfg(feature = "arbitrary")]
+use arbitrary::{Arbitrary, Result, Unstructured};
+use frame_support::dispatch::{Decode, Encode};
 
-pub mod weights;
+#[derive(Clone, Debug, Decode, Encode, Eq, PartialEq)]
+pub enum MultiHash {
+    Sha3_384([u8; 50]),
+}
 
-pub use pallet::*;
-
-#[frame_support::pallet]
-mod pallet {
-    use crate::weights::*;
-    use core::marker::PhantomData;
-    use frame_support::{dispatch::DispatchResultWithPostInfo, traits::IsType, transactional};
-    use frame_system::pallet_prelude::OriginFor;
-    use zeitgeist_primitives::types::MultiHash;
-
-    impl<T: Config> Pallet<T> {
-        pub fn create_categorical_market(
-            _: OriginFor<T>,
-            metadata: MultiHash,
-        ) -> Result<Option<u128>, &'static str> {
-            use frame_support::storage::{with_transaction, TransactionOutcome};
-            with_transaction(|| {
-                let r = (|| {
-                    let MultiHash::Sha3_384(_multihash) = metadata;
-                    Ok(Some(0))
-                })();
-                if r.is_ok() {
-                    TransactionOutcome::Commit(r)
-                } else {
-                    TransactionOutcome::Rollback(r)
-                }
-            })
+pub fn create_categorical_market(
+    metadata: MultiHash,
+) -> Result<Option<u128>, &'static str> {
+    use frame_support::storage::{with_transaction, TransactionOutcome};
+    with_transaction(|| {
+        let r = (|| {
+            let MultiHash::Sha3_384(_multihash) = metadata;
+            Ok(Some(0))
+        })();
+        if r.is_ok() {
+            TransactionOutcome::Commit(r)
+        } else {
+            TransactionOutcome::Rollback(r)
         }
-    }
-
-    #[pallet::config]
-    pub trait Config: frame_system::Config {
-        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-
-        type WeightInfo: WeightInfoZeitgeist;
-    }
-
-    #[pallet::event]
-    pub enum Event<T>
-    where
-        T: Config, {}
-
-    #[pallet::pallet]
-    pub struct Pallet<T>(PhantomData<T>);
+    })
 }
