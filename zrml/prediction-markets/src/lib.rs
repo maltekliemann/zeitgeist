e00adb0735cb61c8772125be0f683b17df629b38
maltekliemann <mail@maltekliemann.com>
2022-04-21 23:43:45 +0200

    big reduction

--- zrml/prediction-markets/src/lib.rs
+++ zrml/prediction-markets/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn with_transaction(f: impl FnOnce() -> i32) -> i32 {
-    f()
+fn g(_: impl FnOnce()) { }
+
+pub enum E {
+    S([u8; 50]),
 }
 
-pub enum MultiHash {
-    Sha3_384([u8; 50]),
-}
-
-pub fn create_categorical_market(metadata: MultiHash) -> i32 {
-    with_transaction(|| {
-        let MultiHash::Sha3_384(_multihash) = metadata;
-        0i32
-    })
+pub fn f(x: E) {
+    g(|| {
+        let E::S(_x) = x;
+    });
 }
