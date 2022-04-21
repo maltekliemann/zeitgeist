5c08c2bb36cd2fa75513553d3ee8e7c7b1ecf660
maltekliemann <mail@maltekliemann.com>
2022-04-21 23:37:24 +0200

    more simplification

--- zrml/prediction-markets/src/lib.rs
+++ zrml/prediction-markets/src/lib.rs
@@ -4,12 +4,9 @@ pub enum MultiHash {
     Sha3_384([u8; 50]),
 }
 
-pub fn create_categorical_market(metadata: MultiHash) -> Result<Option<u128>, &'static str> {
+pub fn create_categorical_market(metadata: MultiHash) -> Option<u128> {
     with_transaction(|| {
-        let r = (|| {
-            let MultiHash::Sha3_384(_multihash) = metadata;
-            Ok(Some(0))
-        })();
-        if r.is_ok() { TransactionOutcome::Commit(r) } else { TransactionOutcome::Rollback(r) }
+        let MultiHash::Sha3_384(_multihash) = metadata;
+        TransactionOutcome::Commit(Some(0))
     })
 }
