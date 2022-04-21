d99032948d5ea17d0dc7d44a4aa7811e12460c6e
maltekliemann <mail@maltekliemann.com>
2022-04-21 23:34:36 +0200

    .

--- zrml/prediction-markets/src/lib.rs
+++ zrml/prediction-markets/src/lib.rs
@@ -4,18 +4,12 @@ pub enum MultiHash {
     Sha3_384([u8; 50]),
 }
 
-pub fn create_categorical_market(
-    metadata: MultiHash,
-) -> Result<Option<u128>, &'static str> {
+pub fn create_categorical_market(metadata: MultiHash) -> Result<Option<u128>, &'static str> {
     with_transaction(|| {
         let r = (|| {
             let MultiHash::Sha3_384(_multihash) = metadata;
             Ok(Some(0))
         })();
-        if r.is_ok() {
-            TransactionOutcome::Commit(r)
-        } else {
-            TransactionOutcome::Rollback(r)
-        }
+        if r.is_ok() { TransactionOutcome::Commit(r) } else { TransactionOutcome::Rollback(r) }
     })
 }
