06b359731c860fdbd789f5cd83c4bdbd23855658
maltekliemann <mail@maltekliemann.com>
2022-04-22 13:11:12 +0200

    .

--- zrml/prediction-markets/src/lib.rs
+++ zrml/prediction-markets/src/lib.rs
@@ -1,11 +1,10 @@
-fn g(_: impl FnOnce()) { }
-
 pub enum E {
     S([u8; 50]),
 }
 
-pub fn f(x: E) {
-    g(|| {
+pub fn f() {
+    let x = E::S([0u8; 50]);
+    || {
         let E::S(_x) = x;
-    });
+    };
 }
